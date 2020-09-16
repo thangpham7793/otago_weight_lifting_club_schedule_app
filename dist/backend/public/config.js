150a1956a89773c63c97ee11c7bf33b1f8d26ea9
Thang <thangnus@gmail.com>
2020-09-16 13:44:10 +1200

    fixed wrong api URL

--- dist/backend/public/config.js
+++ dist/backend/public/config.js
@@ -1,7 +1,7 @@
 const config = (function () {
   "use strict"
 
-  const PROD = "https://lifting-schedule.herokuapp.com"
+  const PROD = "https://lifting-schedule-v2.herokuapp.com"
   const DEV = "http://localhost:3000"
 
   return { URL: location.href.split(".").includes("herokuapp") ? PROD : DEV }
