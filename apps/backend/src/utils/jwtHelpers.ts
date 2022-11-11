774e16d0750ef1e1362177f2049816315fb1a1eb
thang.pham <thang.pham@cogo.co>
2022-11-12 10:46:08 +1300

    use forever test tokens & make makeToken take in options

--- apps/backend/src/utils/jwtHelpers.ts
+++ apps/backend/src/utils/jwtHelpers.ts
@@ -9,14 +9,18 @@ const DAYS_IN_SECONDS = 86400
 
 config({ path: path.resolve(__dirname, "../.env") })
 
-export const makeToken = async (userId: {}) => {
-  const signOptions = {
-    exp: NOW + appConfig.TOKEN_DURATION * DAYS_IN_SECONDS,
-    data: userId,
+export const makeToken = async (
+  payload: {learnerId: number} | {instructorId: number},
+  opts: jwt.SignOptions & { secret?: string } = {}
+) => {
+  const { secret = appConfig.TOKEN_SECRET, ...signOptions } = opts
+  const finalOpts = {
+    expiresIn: `${appConfig.TOKEN_DURATION} days`,
+    ...signOptions,
   }
 
   try {
-    const token = jwt.sign(signOptions, appConfig.TOKEN_SECRET)
+    const token = jwt.sign({ data: payload }, secret, finalOpts)
     return token
   } catch (error) {
     throw new httpError(500, `Error signing token ${error}`)
