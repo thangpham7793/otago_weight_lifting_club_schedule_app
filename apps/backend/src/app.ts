8fef934cad787929d755f2986f832125b4385c09
thang.pham <thang.pham@cogo.co>
2022-11-13 20:00:33 +1300

    rework app to use function instead of Class

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,10 +1,6 @@
 import { serverError, unknownEndpoint } from "./utils"
 
-import {
-  InstructorRouter,
-  ProgrammeRouter,
-  LearnerRouter,
-} from "./routers"
+import { InstructorRouter, ProgrammeRouter, LearnerRouter } from "./routers"
 
 import express, { Application } from "express"
 import bodyParser from "body-parser"
@@ -12,54 +8,44 @@ import cors from "cors"
 import path from "path"
 import morgan from "morgan"
 
-class App {
-  app: Application
-  instructorRouter: InstructorRouter
-  learnerRouter: LearnerRouter
-  scheduleRouter: ProgrammeRouter
+const app = express()
 
-  constructor() {
-    this.app = express()
-    this.setConfig()
-    this.useStatic()
+useConfig(app)
+useStatic(app)
+useRouters(app)
+useErrorHandlers(app)
 
-    this.instructorRouter = new InstructorRouter(this.app)
-    this.learnerRouter = new LearnerRouter(this.app)
-    this.scheduleRouter = new ProgrammeRouter(this.app)
-    this.useErrorHandlers()
-  }
-
-  private setConfig() {
-
-    this.app.use(bodyParser.json({ limit: "50mb" }))
-
-    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
-
-    this.app.use(cors({ credentials: true, origin: "*" }))
-
-    this.app.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*")
-      res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST")
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      )
-      next()
-    })
-
-    this.app.use(
-      morgan(":method :url :status :res[content-length] - :response-time ms")
+function useConfig(app: Application) {
+  app.use(bodyParser.json({ limit: "50mb" }))
+  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
+  app.use(cors({ credentials: true, origin: "*" }))
+  app.use(function (_req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     )
-  }
-
-  private useErrorHandlers() {
-    this.app.use(unknownEndpoint)
-    this.app.use(serverError)
-  }
-
-  private useStatic() {
-    this.app.use(express.static(path.join(__dirname, "public/instructor")))
-  }
+    next()
+  })
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms")
+  )
 }
 
-export default new App().app
+function useStatic(app: Application) {
+  app.use(express.static(path.join(__dirname, "public/instructor")))
+}
+
+function useErrorHandlers(app: Application) {
+  app.use(unknownEndpoint)
+  app.use(serverError)
+}
+
+function useRouters(app: Application) {
+  new InstructorRouter(app)
+  new LearnerRouter(app)
+  new ProgrammeRouter(app)
+}
+
+export default app
