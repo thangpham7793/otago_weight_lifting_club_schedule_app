c8b841cde11f9bc8a25155561532e266a6413150
thang.pham <thang.pham@cogo.co>
2022-11-14 23:31:11 +1300

    centralize jwt helpers

--- apps/backend/src/utils/jwtHelpers.ts
+++ apps/backend/src/utils/jwtHelpers.ts
@@ -3,18 +3,25 @@ import jwt from "jsonwebtoken"
 
 export const makeToken = async (
   payload: { learnerId: number } | { instructorId: number },
-  opts: jwt.SignOptions & { secret?: string } = {}
+  opts: jwt.SignOptions = {}
 ) => {
-  const { secret = appConfig.TOKEN_SECRET, ...signOptions } = opts
-  const finalOpts = {
+  const signOptions = {
     expiresIn: `${appConfig.TOKEN_DURATION} days`,
-    ...signOptions,
+    ...opts,
   }
 
   try {
-    const token = jwt.sign({ data: payload }, secret, finalOpts)
+    const token = jwt.sign(
+      { data: payload },
+      appConfig.TOKEN_SECRET,
+      signOptions
+    )
     return token
   } catch (error) {
     throw new HttpError(500, `Error signing token ${error}`)
   }
+}
+
+export const verifyToken = (token: string) => {
+  return jwt.verify(token, appConfig.TOKEN_SECRET)
 }
