72686fc627b558d0e84602cd38055a24ac572cce
thang.pham <thang.pham@cogo.co>
2022-11-14 23:22:01 +1300

    remove dotenv & simplify appConfig

--- apps/backend/src/database/pool.ts
+++ apps/backend/src/database/pool.ts
@@ -1,4 +1,19 @@
-import { appConfig } from "../utils/config"
-import { Pool } from "pg"
+import { Pool, PoolConfig } from "pg"
+import { appConfig } from "../utils"
 
-export const pool: Pool = new Pool(appConfig.DB_CONFIG)
+const localDbConfig: PoolConfig = {
+  host: appConfig.DOCKERIZED === "true" ? "db" : "0.0.0.0",
+  user: "test_user",
+  port: 5432,
+  database: "lifting",
+  password: "test_user",
+}
+
+const prodDbConfig: PoolConfig = {
+  connectionString: appConfig.DATABASE_URL,
+}
+
+const DB_CONFIG =
+  appConfig.ENVIRONMENT !== "production" ? localDbConfig : prodDbConfig
+
+export const pool = new Pool(DB_CONFIG)
--- apps/backend/src/utils/config.ts
+++ apps/backend/src/utils/config.ts
@@ -1,26 +1,8 @@
-import { config } from "dotenv"
-import { ClientConfig } from "pg"
-import path from "path"
-
-config({ path: path.resolve(__dirname, "../.env") })
-
-const localConfig: ClientConfig = {
-  host: process.env.DOCKER === "true" ? "db" : "0.0.0.0",
-  user: process.env.POSTGRES_USER,
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-}
-
-const prodDbConfig: ClientConfig = {
-  connectionString: process.env.DATABASE_URL || process.env.STORED_DATABASE_URL,
-}
-
 export const appConfig = {
-  TOKEN_DURATION: process.env.TOKEN_DURATION
-    ? Number.parseInt(process.env.TOKEN_DURATION)
-    : 28,
-  TOKEN_SECRET: process.env.SECRET,
+  TOKEN_DURATION: process.env.TOKEN_DURATION ?? 28,
+  TOKEN_SECRET: "secret",
   PORT: Number.parseInt(process.env.PORT) || 8080,
-  DB_CONFIG: process.env.NODE_ENV !== "production" ? localConfig : prodDbConfig,
+  DATABASE_URL: process.env.DATABASE_URL,
+  ENVIRONMENT: process.env.NODE_ENV,
+  DOCKERIZED: process.env.DOCKER,
 }
