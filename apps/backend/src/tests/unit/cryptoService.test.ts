f450b89794766989aa7f94da55627085f76df66c
thang.pham <thang.pham@cogo.co>
2022-11-18 22:46:53 +1300

    use node built-in mods for unit tests

--- apps/backend/src/tests/unit/cryptoService.test.ts
+++ apps/backend/src/tests/unit/cryptoService.test.ts
@@ -1,8 +1,11 @@
 import { compare, hash } from "../../utils/cryptoService"
+import test from "node:test"
+import assert from "node:assert/strict"
 
 test("hashed pw can be used to verify user input", async () => {
   const pw = "foo"
 
   const saved = await hash(pw)
-  await expect(compare(pw, saved)).resolves.toBe(true)
+  const actual = await compare(pw, saved)
+  assert.equal(true, actual)
 })
