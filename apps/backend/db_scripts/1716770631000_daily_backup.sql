5d2ea13482ad22ab7e074d766b26e88af0abb9df
thangpham7793 <thangpham7793@users.noreply.github.com>
2024-05-27 00:44:30 +0000

    daily-backup-202405270043

--- apps/backend/db_scripts/1716770631000_daily_backup.sql
+++ apps/backend/db_scripts/1716770631000_daily_backup.sql
@@ -349,11 +349,11 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 51	Brock	Stewart	Brockstewart2017@gmail.com	55.00	67.00	65.00	65.00	90.00	78.00	55.00	1	stewartb
 64	Lochlan	Webb	Lochlanroganwebb04@gmail.com	82.00	108.00	100.00	100.00	125.00	115.00	83.00	1	webbl
 226	carmi	oosthuizen	carmi.oosthuizen@gmail.com	58.00	75.00	71.50	74.00	95.00	75.00	50.00	1	oosthuizenc
-52	Flook	Chinsaswat	flookchinsaswat@gmail.com	165.00	220.00	198.00	198.00	291.00	276.00	156.00	1	chinsaswatf
 58	Taylor	Hamilton	Taylor.4.hamilton@gmail.com	49.00	58.00	62.00	57.00	78.00	66.00	53.00	1	hamiltont
 232	lochlan rogan	webb	lochlanroganwebb04@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	webbl
 1	thang	pham	thangnus@gmail.com	120.45	120.00	234.00	120.00	120.00	120.00	110.00	1	phamt
 233	test	guy	zeldascott55@gmail.com	20.00	50.00	60.00	70.00	90.00	100.00	60.00	1	guyt
+52	Flook	Chinsaswat	flookchinsaswat@gmail.com	60.00	85.00	70.00	70.00	120.00	110.00	55.00	1	chinsaswatf
 172	cole	jones	jonescoleza@gmail.com	84.00	105.00	113.00	101.00	120.00	110.00	95.00	1	jonesc
 227	miandi	oosthuizen	miandi0508@gmail.com	75.00	92.50	92.60	92.50	120.00	96.00	70.00	1	oosthuizenm
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
