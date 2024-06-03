3d9b6ddec7eea784b82703e57f2e095637e1ef35
thangpham7793 <thangpham7793@users.noreply.github.com>
2024-06-03 00:45:11 +0000

    daily-backup-202406030044

--- apps/backend/db_scripts/1717375474000_daily_backup.sql
+++ apps/backend/db_scripts/1717375474000_daily_backup.sql
@@ -360,6 +360,7 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
 62	Oliver	Rohtmets	Dicksono@kavanagh.school.nz	80.00	107.00	101.00	101.00	125.00	113.00	90.00	1	rohtmetso
 213	julian	harker	athyriel0@gmail.com	100.00	122.00	120.00	118.00	155.00	135.00	106.00	1	harkerj
+215	flynn	cunningham-beentjes	flynn.c.b@icloud.com	71.00	100.00	95.00	90.00	125.00	110.00	90.00	1	cunningham-beentjesf
 218	connell	webb	connelleaganwebb@gmail.com	78.00	105.00	105.00	105.00	120.00	110.00	70.00	1	webbc
 235	ella	rowe	ellagymnastrowe@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	rowee
 236	patrick nevareta	sefo-cloughley	patricksefocloughley@gmail.com	107.00	147.00	145.00	139.00	190.00	150.00	110.00	1	sefo-cloughleyp
@@ -370,7 +371,6 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 66	Patrick	Cloughley	patricksefocloughley11@gmail.com	115.00	150.00	150.00	150.00	200.00	165.00	120.00	1	cloughleyp
 88	Luka	Homersham	Lukahomersham@gmail.com	85.00	110.00	110.00	110.00	160.00	0.00	0.00	1	homershaml
 46	Callan	Helms	callanhelms@gmail.com	120.00	153.00	170.00	153.00	250.00	200.00	145.00	1	helmsc
-215	flynn	cunningham-beentjes	flynn.c.b@icloud.com	71.00	100.00	95.00	90.00	125.00	110.00	90.00	1	cunningham-beentjesf
 \.
 
 
