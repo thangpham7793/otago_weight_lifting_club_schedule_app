4235b2d626a4f21f150928a50b10206331d74b4d
thangpham7793 <thangpham7793@users.noreply.github.com>
2025-01-29 00:53:57 +0000

    daily-backup-202501290053

--- apps/backend/db_scripts/1738112010000_daily_backup.sql
+++ apps/backend/db_scripts/1738112010000_daily_backup.sql
@@ -360,8 +360,8 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 232	lochlan rogan	webb	lochlanroganwebb04@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	webbl
 1	thang	pham	thangnus@gmail.com	120.45	120.00	234.00	120.00	120.00	120.00	110.00	1	phamt
 233	test	guy	zeldascott55@gmail.com	20.00	50.00	60.00	70.00	90.00	100.00	60.00	1	guyt
-224	jeremy	manteiga	jeremymanteiga@gmail.com	85.00	115.00	105.00	108.00	160.00	130.00	90.00	1	manteigaj
 172	cole	jones	jonescoleza@gmail.com	84.00	105.00	113.00	101.00	120.00	110.00	95.00	1	jonesc
+224	jeremy	manteiga	jeremymanteiga@gmail.com	85.00	115.00	105.00	108.00	150.00	130.00	90.00	1	manteigaj
 227	miandi	oosthuizen	miandi0508@gmail.com	75.00	92.50	92.60	92.50	120.00	96.00	70.00	1	oosthuizenm
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
 213	julian	harker	athyriel0@gmail.com	100.00	122.00	120.00	118.00	155.00	135.00	106.00	1	harkerj
