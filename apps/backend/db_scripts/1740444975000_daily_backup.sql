9a6d7ff95b60f9d344813aac5574703a72f46756
thangpham7793 <thangpham7793@users.noreply.github.com>
2025-02-25 00:56:42 +0000

    daily-backup-202502250056

--- apps/backend/db_scripts/1740444975000_daily_backup.sql
+++ apps/backend/db_scripts/1740444975000_daily_backup.sql
@@ -361,10 +361,10 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 1	thang	pham	thangnus@gmail.com	120.45	120.00	234.00	120.00	120.00	120.00	110.00	1	phamt
 233	test	guy	zeldascott55@gmail.com	20.00	50.00	60.00	70.00	90.00	100.00	60.00	1	guyt
 172	cole	jones	jonescoleza@gmail.com	84.00	105.00	113.00	101.00	120.00	110.00	95.00	1	jonesc
-224	jeremy	manteiga	jeremymanteiga@gmail.com	85.00	115.00	105.00	108.00	150.00	130.00	90.00	1	manteigaj
 227	miandi	oosthuizen	miandi0508@gmail.com	75.00	92.50	92.60	92.50	120.00	96.00	70.00	1	oosthuizenm
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
 213	julian	harker	athyriel0@gmail.com	100.00	122.00	120.00	118.00	155.00	135.00	106.00	1	harkerj
+224	jeremy	manteiga	jeremymanteiga@gmail.com	85.00	120.00	110.00	110.00	155.00	135.00	90.00	1	manteigaj
 218	connell	webb	connelleaganwebb@gmail.com	78.00	105.00	105.00	105.00	120.00	110.00	70.00	1	webbc
 235	ella	rowe	ellagymnastrowe@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	rowee
 236	patrick nevareta	sefo-cloughley	patricksefocloughley@gmail.com	107.00	147.00	145.00	139.00	190.00	150.00	110.00	1	sefo-cloughleyp
@@ -679,7 +679,7 @@ SELECT pg_catalog.setval('public."instructor_instructorId_seq"', 2, true);
 -- Name: learner_learnerId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."learner_learnerId_seq"', 237, true);
+SELECT pg_catalog.setval('public."learner_learnerId_seq"', 238, true);
 
 
 --
