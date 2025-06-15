e4629262724b0888c4c9ae45445cf00d6b5ef258
thangpham7793 <thangpham7793@users.noreply.github.com>
2025-06-15 01:12:28 +0000

    daily-backup-202506150111

--- apps/backend/db_scripts/1749949917000_daily_backup.sql
+++ apps/backend/db_scripts/1749949917000_daily_backup.sql
@@ -364,7 +364,6 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 227	miandi	oosthuizen	miandi0508@gmail.com	75.00	92.50	92.60	92.50	120.00	96.00	70.00	1	oosthuizenm
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
 213	julian	harker	athyriel0@gmail.com	100.00	122.00	120.00	118.00	155.00	135.00	106.00	1	harkerj
-224	jeremy	manteiga	jeremymanteiga@gmail.com	94.00	120.00	115.00	115.00	160.00	140.00	90.00	1	manteigaj
 62	Oliver	Rohtmets	Dicksono@kavanagh.school.nz	81.00	113.00	101.00	101.00	140.00	120.00	90.00	1	rohtmetso
 218	connell	webb	connelleaganwebb@gmail.com	78.00	105.00	105.00	105.00	120.00	110.00	70.00	1	webbc
 235	ella	rowe	ellagymnastrowe@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	rowee
@@ -373,6 +372,7 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 225	jumari	oosthuizen	jumari.oosthuizen@gmail.com	72.00	93.00	93.00	93.00	120.00	103.00	68.00	1	oosthuizenj
 219	john	zhao	johnzhao2612@gmail.com	90.00	120.00	110.00	120.00	200.00	180.00	85.00	1	zhaoj
 221	johm	zhao	johnzhao2003@icloud.com	90.00	138.00	120.00	120.00	200.00	170.00	95.00	1	zhaoj
+224	jeremy	manteiga	jeremymanteiga@gmail.com	96.00	120.00	118.00	118.00	160.00	140.00	90.00	1	manteigaj
 135	luka	homersham	lukahomersham@gmail.com	85.00	115.00	115.00	115.00	160.00	125.00	92.50	1	homershaml
 66	Patrick	Cloughley	patricksefocloughley11@gmail.com	115.00	150.00	150.00	150.00	200.00	165.00	120.00	1	cloughleyp
 215	flynn	cunningham-beentjes	flynn.c.b@icloud.com	71.00	100.00	95.00	90.00	137.00	110.00	85.00	1	cunningham-beentjesf
