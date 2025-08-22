fc63b3c0623dcf555bee675ba2635e35e93dd059
thangpham7793 <thangpham7793@users.noreply.github.com>
2025-08-22 01:01:48 +0000

    daily-backup-202508220101

--- apps/backend/db_scripts/1755824479000_daily_backup.sql
+++ apps/backend/db_scripts/1755824479000_daily_backup.sql
@@ -2,8 +2,10 @@
 -- PostgreSQL database dump
 --
 
+\restrict 8evj1VASBraiM8yuxv0SKYz1nLYO2wQf32rzMz2vUNQHKF6Qng9H4uvnwtVgLkw
+
 -- Dumped from database version 14.6 (Debian 14.6-1.pgdg110+1)
--- Dumped by pg_dump version 16.9 (Ubuntu 16.9-1.pgdg24.04+1)
+-- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -850,4 +852,6 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 --
 -- PostgreSQL database dump complete
 --
+
+\unrestrict 8evj1VASBraiM8yuxv0SKYz1nLYO2wQf32rzMz2vUNQHKF6Qng9H4uvnwtVgLkw
 
