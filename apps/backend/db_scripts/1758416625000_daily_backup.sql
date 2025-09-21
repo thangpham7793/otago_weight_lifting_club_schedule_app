33939bc5aef9fcaf3062f686751b520b73ac3c58
thangpham7793 <thangpham7793@users.noreply.github.com>
2025-09-21 01:04:14 +0000

    daily-backup-202509210103

--- apps/backend/db_scripts/1758416625000_daily_backup.sql
+++ apps/backend/db_scripts/1758416625000_daily_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict Wxh2ByMhmFPCQrjze7pW6ezGdDWklHZmLMtGhHiXQcpfdTjmU4WepevOJz8gmCs
+\restrict jdAZgNfTexfCFwvUmt51RJBb3Doj85DXOI9qpxSerax4dKWbnMAkYA9B7SqmCgg
 
 -- Dumped from database version 14.6 (Debian 14.6-1.pgdg110+1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
@@ -361,6 +361,7 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 232	lochlan rogan	webb	lochlanroganwebb04@gmail.com	0.00	0.00	0.00	0.00	0.00	0.00	0.00	1	webbl
 1	thang	pham	thangnus@gmail.com	120.45	120.00	234.00	120.00	120.00	120.00	110.00	1	phamt
 233	test	guy	zeldascott55@gmail.com	20.00	50.00	60.00	70.00	90.00	100.00	60.00	1	guyt
+224	jeremy	manteiga	jeremymanteiga@gmail.com	98.00	120.00	120.00	120.00	165.00	140.00	95.00	1	manteigaj
 172	cole	jones	jonescoleza@gmail.com	84.00	105.00	113.00	101.00	120.00	110.00	95.00	1	jonesc
 227	miandi	oosthuizen	miandi0508@gmail.com	75.00	92.50	92.60	92.50	120.00	96.00	70.00	1	oosthuizenm
 171	poppy	kirk	pbkirk16@gmail.com	60.00	72.00	76.00	71.00	100.00	80.00	57.00	1	kirkp
@@ -377,7 +378,6 @@ COPY public.learner ("learnerId", "firstName", "lastName", email, snatch, clean,
 88	Luka	Homersham	Lukahomersham@gmail.com	85.00	110.00	110.00	110.00	160.00	0.00	0.00	1	homershaml
 46	Callan	Helms	callanhelms@gmail.com	120.00	153.00	170.00	153.00	250.00	200.00	145.00	1	helmsc
 52	Flook	Chinsaswat	flookchinsaswat@gmail.com	70.00	100.00	88.00	88.00	133.00	118.00	55.00	1	chinsaswatf
-224	jeremy	manteiga	jeremymanteiga@gmail.com	96.00	120.00	118.00	118.00	160.00	140.00	95.00	1	manteigaj
 58	Taylor	Hamilton	Taylor.4.hamilton@gmail.com	40.00	50.00	50.00	50.00	65.00	55.00	45.00	1	hamiltont
 62	Oliver	Rohtmets	Dicksono@kavanagh.school.nz	85.00	115.00	102.00	102.00	140.00	120.00	90.00	1	rohtmetso
 \.
@@ -853,5 +853,5 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict Wxh2ByMhmFPCQrjze7pW6ezGdDWklHZmLMtGhHiXQcpfdTjmU4WepevOJz8gmCs
+\unrestrict jdAZgNfTexfCFwvUmt51RJBb3Doj85DXOI9qpxSerax4dKWbnMAkYA9B7SqmCgg
 
