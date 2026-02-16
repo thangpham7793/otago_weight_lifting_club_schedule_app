431cc1bbf1bca260d8b0764eef303e457df00899
thangpham7793 <thangpham7793@users.noreply.github.com>
2026-02-16 01:25:23 +0000

    daily-backup-202602160124

--- apps/backend/db_scripts/1771205095000_daily_backup.sql
+++ apps/backend/db_scripts/1771205095000_daily_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict obCebTOlnly66WVGXVnFLRBKg4JTH3m2Ef7oyg4NM7yg3ANffX5Va3iFf4TtDqb
+\restrict 7QjYThzGnUFOV0b3jDka77XuiSUveONt27eWQIMYgfdI4Od2ThH9muxGAERcgBC
 
 -- Dumped from database version 14.6 (Debian 14.6-1.pgdg110+1)
 -- Dumped by pg_dump version 16.11 (Ubuntu 16.11-1.pgdg24.04+1)
@@ -856,5 +856,5 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict obCebTOlnly66WVGXVnFLRBKg4JTH3m2Ef7oyg4NM7yg3ANffX5Va3iFf4TtDqb
+\unrestrict 7QjYThzGnUFOV0b3jDka77XuiSUveONt27eWQIMYgfdI4Od2ThH9muxGAERcgBC
 
